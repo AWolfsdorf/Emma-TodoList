9421851666fe5d31d0d15a5361b73bf1fe1a44df
Alexis Wolfsdorf <alexis@thinkanddev.com>
2022-01-08 21:55:45 -0300

    Listando usuarios y luego las tareas de cada uno

--- ToDoList/AppList/views.py
+++ ToDoList/AppList/views.py
@@ -7,4 +7,6 @@ from django.views import generic
 class IndexView(generic.ListView):
     model = UserProfile
     template_name = 'AppList/index.html'
+    # Agregando la siguiente linea le cambias el nombre del 'object_list'
+    # context_object_name = 'user_profiles'
     
